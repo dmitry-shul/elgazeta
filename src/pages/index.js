ea0b2c4416552470020a293bb461a70f827d4cd6
Dmitry Shulzhytski <70850491+dmitry-shul@users.noreply.github.com>
2023-08-02 12:21:02 +0000

    add images

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import styles from "@/styles/styles.module.css"
 import Head from "next/head"
+import Image from "next/image"
 
 export default function Main() {
   return (
@@ -43,17 +44,37 @@ const ArchiveButton = () => {
 const RubricsList = () => {
   return (
     <div className={styles.rubrics}>
-      <Rubric />
+      {
+        rubricsArray.map(item =>
+          <Rubric item={item} />   
+        )
+      }
     </div>
   )
 }
 
 
 
-const Rubric = () => {
+const Rubric = ({item}) => {
+
+  const {link, width, height, text} = item
+
   return (
-    <div>
-      787u
+    <div className={styles.rubric} style={{backgroundImage: `url(${link})`, width, height}}>
+      <div className={styles.rubric__border}>
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
+
+
+
+const rubricsArray = [
+  {id: 1, link: '/assets/images/1rub.jpg', width: "564px", height: "336px", text: "В ЦЕНТРЕ ВНИМАНИЯ"},
+  {id: 2, link: '/assets/images/2rub.jpg', width: "362px", height: "336px", text: "ХОББИ-ТЫ"},
+  {id: 3, link: '/assets/images/3rub.jpg', width: "374px", height: "312px", text: "ДЕНЬ В КАЛЕНДАРЕ"},
+  {id: 4, link: '/assets/images/4rub.jpg', width: "558px", height: "312px", text: "РОВЕСНИК"},
+  {id: 5, link: '/assets/images/5rub.jpg', width: "600px", height: "304px", text: "В МИРЕ ПРОФЕССИЙ"},
+  {id: 6, link: '/assets/images/6rub.jpg', width: "316px", height: "304px", text: "БЛОКНОТ"},
+]
