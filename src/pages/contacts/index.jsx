48fa2cfb11b7b5dd1d592e86e5a84723f6a5b199
Dmitry Shulzhytski <70850491+dmitry-shul@users.noreply.github.com>
2023-08-03 06:34:42 +0000

    add Contacts page

--- src/pages/contacts/index.jsx
+++ src/pages/contacts/index.jsx
@@ -9,7 +9,18 @@ const Contacts = () => {
         <title>Контакты - Маладзік</title>
       </Head>
 
-      ddd
+      <div className="container">
+        <div className={styles.contacts}>
+          <h1>КОНТАКТЫ</h1>
+
+          <div className={styles.info}>
+            <span><b>Молодечненский центр творчества детей и молодежи "Маладик"</b></span>
+            <span>Адрес: 222310, Минская обл., г. Молодечно, ул. Машерова, 3</span>
+            <span>Телефон: 8 (0176) 58-05-33</span>
+            <span>E-mail: <a href="mailto:rgmaladzik@gmail.com">rgmaladzik@gmail.com</a></span>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
