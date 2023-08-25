e0c0131e2c6e6bb443b39303aae7a87c0e92ae1f
Dmitry <70850491+dmitry-shul@users.noreply.github.com>
2023-08-25 10:08:01 +0000

    fix n15

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  output: "export",
+  distDir: "_static",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
