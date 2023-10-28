2a6e5f6648cdda90991fdfcf0dfe0ede0a926b8c
Dmitry Shulzhytski <70850491+dmitry-shul@users.noreply.github.com>
2023-10-28 13:48:46 +0300

    fix rubricsArray

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,9 +77,9 @@ const Rubric = ({item, ...props}) => {
 
 const rubricsArray = [
   {id: 1, link: '/assets/images/1rub.jpg', width: "564px", height: "336px", text: "В ЦЕНТРЕ ВНИМАНИЯ", positionX: "center", positionX: "center"},
-  {id: 3, link: '/assets/images/3rub.webp', width: "374px", height: /*"312px"*/ "336px", text: "ДЕНЬ В КАЛЕНДАРЕ", positionX: "center", positionY: "center"},
-  {id: 6, link: '/assets/images/6rub.jpg', width: /*"316px"*/ "372px", height: /*"304px*/ "312px", text: "ИНТЕРВЬЮ", positionX: "center", positionY: "center"},
-  {id: 4, link: '/assets/images/4rub.jpg', width: "558px", height: "312px", text: "РОВЕСНИК", positionX: "center", positionY: "top"},
-  /*{id: 2, link: '/assets/images/2rub.jpg', width: "362px", height: "336px", text: "ХОББИ-ТЫ", positionX: "center", positionY: "top"},
-  {id: 5, link: '/assets/images/5rub.jpg', width: "600px", height: "304px", text: "В МИРЕ ПРОФЕССИЙ", positionX: "center", positionY: "top"},*/
+  {id: 3, link: '/assets/images/3rub.webp', width: "374px", height: "336px", text: "ДЕНЬ В КАЛЕНДАРЕ", positionX: "center", positionY: "center"},
+  {id: 2, link: '/assets/images/newrub2.jpg', width: "362px", height: "330px", text: "ХОББИ-ТЫ", positionX: "center", positionY: "top"},
+  {id: 4, link: '/assets/images/4rub.jpg', width: "570px", height: "330px", text: "РОВЕСНИК", positionX: "center", positionY: "top"},
+  {id: 6, link: '/assets/images/newrub1.jpg', width: "564px", height: "312px", text: "ИНТЕРВЬЮ", positionX: "center", positionY: "center"},
+  {id: 5, link: '/assets/images/6rub.jpg', width: "372px", height: "312px", text: "БЛОКНОТ", positionX: "center", positionY: "top"},
 ]
